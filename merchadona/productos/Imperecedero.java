f63661627850b4c2b06874ded4fd1ddbb73e8c4d
pablopg23 <49436766+pablopg23@users.noreply.github.com>
2019-05-26 01:57:45 +0200

    Commit by Pablo
    Commit by Pablo

--- merchadona/productos/Imperecedero.java
+++ merchadona/productos/Imperecedero.java
@@ -1,22 +1,33 @@
 package merchadona.productos;
-
+/**
+ * 
+ * @author Jorge y Javi
+ * @version 05/2019
+ * @since 05/16/2019
+ *
+ */
 public class Imperecedero extends Producto {
-
-	// Atributos
-
-	// No tiene atributos específicos
-
-	// Metodos
-
+	
+	//Atributos
+	
+	 //No tiene atributos específicos
+	
+	//Metodos
+	
+	
 	/**
-	 * @param nombre
-	 * @param precio
+	 * Constructor de la clase Imperecedero que hereda de Producto
+	 * @param nombre : nombre del producto
+	 * @param precio : precio del producto
 	 */
 	public Imperecedero(String nombre, double precio) {
 		super(nombre, precio);
 		// TODO Auto-generated constructor stub
 	}
-
+	/**
+	 * Metodo que aniade la cantidad de producto pasada por parametro
+	 * @param cantidad : cantidad de producto que se aniade
+	 */
 	public void aniadirCantidad(int cantidad) {
 		this.cantidad += cantidad;
 	}
